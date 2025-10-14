0fb63fc199703ad08496c0094e191a0dec386692
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-10-14 12:49:16 +0530

    .

--- Bookmyshow/Bookmyshow/settings.py
+++ Bookmyshow/Bookmyshow/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1nsrk*qtz(rm$#=plbej@ik)0p-r6__3vlu1^6i1gj_=nz=&!7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
