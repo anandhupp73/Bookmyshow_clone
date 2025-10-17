a7ed3dfdc4b2d8a637af74df5f939f627c8e437b
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-10-17 13:05:03 +0530

    mail

--- Bookmyshow/Bookmyshow/settings.py
+++ Bookmyshow/Bookmyshow/settings.py
@@ -126,3 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your_email@example.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'  # Use App Password if Gmail
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
