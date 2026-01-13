79fd4d24e572fa8a3f0b06540f8072830bb81adb
anandhupp73 <anandhakrishnanpp73@gmail.com>
2026-01-13 13:04:41 +0530

    env

--- Bookmyshow/Bookmyshow/settings.py
+++ Bookmyshow/Bookmyshow/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -164,8 +165,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anandhupp67@gmail.com'
-EMAIL_HOST_PASSWORD = 'bxlv imup dmca fbgz'  # Use App Password if Gmail
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
