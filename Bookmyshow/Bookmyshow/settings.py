e5b1abd5e77a88265beeeab3317e66f64d30b638
anandhupp73 <anandhakrishnanpp73@gmail.com>
2026-01-19 14:43:31 +0530

    .

--- Bookmyshow/Bookmyshow/settings.py
+++ Bookmyshow/Bookmyshow/settings.py
@@ -165,8 +165,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'anandhupp67@gmail.com'
+EMAIL_HOST_PASSWORD = 'bxlv imup dmca fbgz'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
