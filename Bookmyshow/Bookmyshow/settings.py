3ebd9df6b4541ab8f8bbba91b74687b5e9756fb5
anandhupp73 <anandhakrishnanpp73@gmail.com>
2026-01-27 15:40:29 +0530

    test

--- Bookmyshow/Bookmyshow/settings.py
+++ Bookmyshow/Bookmyshow/settings.py
@@ -163,7 +163,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.pythonanywhere.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anandhupp67@gmail.com'
 EMAIL_HOST_PASSWORD = 'bxlv imup dmca fbgz'
